24b34b72b0c1e9885f5cd943ef617e01c06e6f3b
mmartinez <mmartinez@dosalcubo.com>
2018-12-18 11:55:17 -0300

    dist

---
+++ dist/precache-manifest.5e977ac812293de6f11ce7102e2be016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "271aa4c66b969d1fe5f3",
+    "url": "/static/js/main.271aa4c6.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "271aa4c66b969d1fe5f3",
+    "url": "/static/css/main.2bdf7879.chunk.css"
+  },
+  {
+    "revision": "29bc156b04e50139ad96f656c9303feb",
+    "url": "/index.html"
+  }
+];

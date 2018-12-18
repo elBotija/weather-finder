781668d4418be147033ce5e9f616fbd028a5847b
mmartinez <mmartinez@dosalcubo.com>
2018-12-18 12:09:42 -0300

    Updates

---
+++ precache-manifest.9fe5f285152362623b11c3d983e5b071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31cd7c94fea27966ddb3",
+    "url": "/weather-finder/static/js/runtime~main.31cd7c94.js"
+  },
+  {
+    "revision": "4d0c7247335b7026f6d4",
+    "url": "/weather-finder/static/js/main.4d0c7247.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/weather-finder/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "4d0c7247335b7026f6d4",
+    "url": "/weather-finder/static/css/main.2bdf7879.chunk.css"
+  },
+  {
+    "revision": "d43a4dd7166785867be81537fc660731",
+    "url": "/weather-finder/index.html"
+  }
+];
